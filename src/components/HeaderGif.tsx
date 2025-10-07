79e0e205f4cc5c774e9253d9c84df0408bf6af6a
Lucas Caballero <caballerolucas18@gmail.com>
2025-10-07 20:32:15 -0300

    second

--- src/components/HeaderGif.tsx
+++ src/components/HeaderGif.tsx
@@ -1,14 +1,13 @@
 interface Props {
-    title: string;
-    description ?: string;
+  title: string;
+  description?: string;
 }
 
-
-export default function HeaderGif({title, description}: Props) {
+export default function HeaderGif({ title, description }: Props) {
   return (
-    <div className='content-center'>
-        <h1>{title}</h1>
-        {/* <p>{description}</p> */}
+    <div className="content-center">
+      <h1>{title}</h1>
+      {/* <p>{description}</p> */}
     </div>
-  )
+  );
 }
